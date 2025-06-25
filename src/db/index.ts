f56db48da357f4bc5a2106d21b7e1e5485eecb30
yousef zair <yousef.zair@student.kdg.be>
2025-06-25 12:00:22 +0200

    02:database

---
+++ src/db/index.ts
@@ -0,0 +1,12 @@
+import { drizzle } from 'drizzle-orm/neon-http';
+
+export const db =drizzle(process.env.DATABASE_URL!);
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
+
